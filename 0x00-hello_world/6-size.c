3ffe5f3558549d996027f8287d7dc1b9b59218f8
SekiNatasha <natashaeffie1@gmail.com>
2022-10-18 07:02:19 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Entry
- *  Return: Always 0
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -13,4 +14,5 @@ int main(void)
 	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
+
 }
