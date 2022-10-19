827ccbd7382600f93ac2b0a92b67094b216f31f0
SekiNatasha <natashaeffie1@gmail.com>
2022-10-19 12:44:29 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,12 @@
  * print_alphabet - prints the alphabet in lower case
  */
 void print_alphabet(void);
-{
+({
 	char la;
 
 	for (la = 'a'; la <= 'z'; la++)
 	{
-		_putchar('la');
+		_putchar(la);
 	}
 	_putchar('\n');
 }
